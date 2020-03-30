75d3724694d4202bf545cf4f3a12b5a810591e1e
JensDN <57954369+JensDN@users.noreply.github.com>
2020-03-30 12:06:44 +0200

    Added cart-hidden flux pattern

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,10 +6,11 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/Logo.svg';
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 import './header.styles.scss';
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -32,9 +33,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon/>
     </div>
+      {
+          hidden ? null : <CartDropDown/>
+      }
   </div>
 );
-const mapStateToProps = state => ({
- currentUser: state.user.currentUser
+const mapStateToProps = ({user : {currentUser}, cart: {hidden}}) => ({
+ currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
