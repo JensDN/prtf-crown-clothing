566730e44dd0d808c32a8fcbc3baf6d565a2dd57
JensDN <57954369+JensDN@users.noreply.github.com>
2020-03-30 18:03:17 +0200

    updating our application with memo selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import CustomButton from "../custom-button/custom-button.component";
 import './cart-dropdown.styles.scss';
 import CartItem from "../cart-item/cart-item.component";
 import {connect} from 'react-redux';
+import { selectCartItems} from '../../redux/cart/cart.selectors'
 const CartDropDown =({cartItems}) => (
     <div className={'cart-dropdown'}>
         <div className="cart-items">
@@ -15,7 +16,7 @@ const CartDropDown =({cartItems}) => (
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
-const mapStateToProps = ({cart : {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropDown)
