a248a2f67c541f5887c0fd27d0015fde1427ffa5
JensDN <57954369+JensDN@users.noreply.github.com>
2020-03-31 16:04:11 +0200

    made working checkout page with total

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Switch, Route, Redirect  } from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
+import {selectCurrentUser} from './redux/user/user.selectors';
+import {createStructuredSelector} from "reselect";
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
@@ -44,13 +47,14 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
   }
 }
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 const mapDispatchToProps = dispatch => ({
 setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,3 +12,15 @@ const selectCart = state => state.cart;
              accumulatedQuantity + cartItem.quantity,0
          )
     );
+ export const selectCartHidden = createSelector(
+     [selectCart],
+     cart=> cart.hidden
+ );
+ export const selectCartTotal = createSelector(
+     [selectCartItems],
+     (cartItems) =>
+         cartItems.reduce((
+             accumulatedQuantity, cartItem) =>
+             accumulatedQuantity + cartItem.quantity * cartItem.price,0
+         )
+ );
